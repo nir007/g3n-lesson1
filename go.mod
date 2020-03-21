f94084cf990d4117cec91e5974a84ae44c3c18b3
Roman <sitius.ru@gmail.com>
2020-03-21 17:30:31 +0300

    init

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/nir007/g3n
+
+go 1.14
+
+require (
+	github.com/g3n/engine v0.1.0 // indirect
+	github.com/go-gl/glfw v0.0.0-20200222043503-6f7a984d4dc4 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	golang.org/x/image v0.0.0-20200119044424-58c23975cae1 // indirect
+	gopkg.in/yaml.v2 v2.2.8 // indirect
+)
---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"github.com/g3n/engine/app"
+	"github.com/g3n/engine/camera"
+	"github.com/g3n/engine/core"
+	"github.com/g3n/engine/geometry"
+	"github.com/g3n/engine/gls"
+	"github.com/g3n/engine/graphic"
+	"github.com/g3n/engine/gui"
+	"github.com/g3n/engine/math32"
+	"github.com/g3n/engine/material"
+	"github.com/g3n/engine/renderer"
+	"github.com/g3n/engine/texture"
+	"github.com/g3n/engine/util/helper"
+	"time"
+)
+
+func main()  {
+	a := app.App()
+	scene := core.NewNode()
+	gui.Manager().Set(scene)
+	// Create perspective camera
+	cam := camera.New(1)
+	cam.SetPosition(0, 0, 4)
+	scene.Add(cam)
+	// Set up orbit control for the camera
+	camera.NewOrbitControl(cam)
+
+	txSpace, err := texture.NewTexture2DFromImage("./textures/box.jpg")
+	if err != nil {
+		panic(err)
+	}
+
+	gTour := geometry.NewBox(2, 2, 2)
+	mat := material.NewStandard(math32.NewColor("White"))
+	mat.AddTexture(txSpace)
+
+	cube := graphic.NewMesh(gTour, mat)
+	cube.RotateX(90)
+	cube.RotateY(40)
+	cube.RotateZ(-80)
+
+	scene.Add(cube)
+
+	// Create and add an axis helper to the scene
+	scene.Add(helper.NewAxes(0.5))
+
+	// Set background color to gray
+	a.Gls().ClearColor(0.5, 0.5, 0.5, 1.0)
+
+	// Run the application
+	a.Run(func(renderer *renderer.Renderer, deltaTime time.Duration) {
+		a.Gls().Clear(gls.DEPTH_BUFFER_BIT | gls.STENCIL_BUFFER_BIT | gls.COLOR_BUFFER_BIT)
+		renderer.Render(scene, cam)
+	})
+}
